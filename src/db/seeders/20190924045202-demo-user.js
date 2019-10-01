62bb397e17dfe146a74fee532326b0c52fcfb996
Lalo <lalo0892@gmail.com>
2019-09-30 19:53:48 -0500

    se actualiza el seeder

--- src/db/seeders/20190924045202-demo-user.js
+++ src/db/seeders/20190924045202-demo-user.js
@@ -6,7 +6,7 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [{
       id: '7f0b850a-2978-4e0d-8b82-d988c74c9a3d',
       username: 'barservices',
-      password: bcrypt.hashSync('lalo', bcrypt.genSaltSync(8)),
+      password: bcrypt.hashSync('b@rS3rv1s3s', bcrypt.genSaltSync(8)),
       nombre: 'Bar',
       apellido: 'Services',
       roleId: '2b59dfc7-cfa7-4791-92bd-7f1bf3264e81',
