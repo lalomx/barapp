1e568829a72dfb2ccb352270be6bb1158e610256
Lalo <lalo0892@gmail.com>
2019-10-07 20:34:39 -0500

    se actualiza nav

--- barappui/src/app/app-routing.module.ts
+++ barappui/src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InitComponent } from './init/init.component';
 import { ComandasComponent } from './comandas/comandas.component';
+import { ReportesComponent } from './reportes/reportes.component';
+import { MenuComponent } from './menu/menu.component';
+import { AlertaComponent } from './alerta/alerta.component';
+import { InventarioComponent } from './inventario/inventario.component';
 
 
 const routes: Routes = [
@@ -12,6 +16,10 @@ const routes: Routes = [
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'comandas', component: ComandasComponent },
+      { path: 'inventario', component: InventarioComponent },
+      { path: 'alertas', component: AlertaComponent },
+      { path: 'menus', component: MenuComponent },
+      { path: 'reportes', component: ReportesComponent },
       { path: '**', pathMatch: 'full', redirectTo: 'dashboard', data: { isPublic: true } }
     ]
   },
