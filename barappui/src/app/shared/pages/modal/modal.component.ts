04c590b4a26fa45c591972919b3652c36a10c5b2
Lalo <lalo0892@gmail.com>
2019-10-23 00:39:43 -0500

    binded events

--- barappui/src/app/shared/pages/modal/modal.component.ts
+++ barappui/src/app/shared/pages/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, AfterViewInit } from '@angular/core';
 import * as UIkit from 'uikit';
 
 
@@ -7,20 +7,41 @@ import * as UIkit from 'uikit';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.less']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, AfterViewInit {
 
   @ViewChild('modal', { static: true}) modal: ElementRef;
+
+  @Input() mode: string;
+  @Input() entity: any;
+  @Input() formMetadata: any;
+
+  private modalObject: any;
 
   constructor() { }
 
   open = false;
 
+  get title() {
+    if (this.mode === 'add') {
+      return 'Nuevo';
+    } else if (this.mode === 'edit') {
+      return 'Editar';
+    } else {
+      return 'Aqui va un titulo';
+    }
+  }
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    this.modalObject = UIkit.modal(this.modal.nativeElement);
+    UIkit.util.on(this.modal.nativeElement, 'shown', () => console.log('shown'));
+    UIkit.util.on(this.modal.nativeElement, 'hidden', () => console.log('hidden'));
   }
 
   show() {
     this.open = true;
-    setTimeout(() => UIkit.modal(this.modal.nativeElement).show(), 500);
+    setTimeout(() => this.modalObject.show(), 500);
   }
 
 }
